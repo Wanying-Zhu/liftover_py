537ac529fab771afbc8f97bfb2f680b60cfc9315
wanying <wanying.zhu.1@vanderbilt.edu>
2023-01-16 23:01:38 -0600

    Allow input of any txt file, use tab as delimiter

--- src/formatting_plink_bim_to_UCSC_bed.py
+++ src/formatting_plink_bim_to_UCSC_bed.py
@@ -7,6 +7,9 @@
 import pandas as pd
 from process_args import process_args
 import subprocess
+import os
+
+args = process_args()
 
 def progress_bar(cur:int, total:int):
     '''
@@ -14,7 +17,7 @@ def progress_bar(cur:int, total:int):
     '''
     percent = 100*cur/total
     progress = '=' * int(percent) + '>' + '-' * (99-int(percent))
-    print(f'\r|{progress}| {percent:.2f}%', end='', flush=True)
+    print(f'|{progress}| {percent:.2f}%', end='\r', flush=True)
 
 
 def plink_bim_to_ucsc_bed(input_fn, output_fn):
@@ -23,7 +26,7 @@ def plink_bim_to_ucsc_bed(input_fn, output_fn):
     '''
     # Get total number of lines of input file
     print('#Check number length of in put file')
-    total = int(subprocess.run(f'wc -l {input_fn}'.split(), text=True).stdout.split()[0])
+    total = int(subprocess.run(f'wc -l {input_fn}'.split(), text=True, capture_output=True).stdout.split()[0])
     print(f'# - input file has {total} entries')
     output_fh = open(output_fn, 'w')
     with open(input_fn, 'r') as input_fh:
@@ -43,8 +46,7 @@ def plink_bim_to_ucsc_bed(input_fn, output_fn):
             if count%5000 == 0:
                 progress_bar(count, total)   # Keep console busy
             line = input_fh.readline().strip()
-        print('\n#Done')
-
+        progress_bar(1, 1) # Print DONE progress bar
 
 def ucsc_bed_to_plink_bim(input_bed_fn, input_bim_fn, output_fn):
     '''
@@ -69,24 +71,117 @@ def ucsc_bed_to_plink_bim(input_bed_fn, input_bim_fn, output_fn):
     df_merge[output_columns].to_csv(output_fn, sep='\t', header=None, index=False)
     print('#Done')
 
+def txt_to_plink_bim(input_fn, output_fn):
+    '''
+    Convert general text file to pseudo plink .bim file. Columns should be arranged as below:
+    - Chromosome code (either an integer, or 'X'/'Y'/'XY'/'MT'; '0' indicates unknown) or name
+    - Variant identifier
+    - Position in morgans or centimorgans (safe to use dummy value of '0')
+    - Base-pair coordinate
+    This pseudo .bim file will be deleted after conversion
+    '''
+    df = pd.read_csv(input_fn, sep='\t', header=None)
+    idx_chr, idx_id, idx_pos = args.chr_id_pos.split(':') # get indices of columns
+    idx_chr, idx_id, idx_pos = int(idx_chr), int(idx_id), int(idx_pos)
 
-def run_liftover():
-    args = process_args() # Parse user input args
+    # Check format of chromosome number
+    # If chromosome number is not integer, strip the sting and only keep the number
+    # Eg. Change CHR1 to 1
+    #TODO:
+    # WZ has not tested this feature out with corner cases.
+    chr_val = df[idx_chr][0]
+    try:
+        chr_val = int(chr_val)
+    except: # Need to use regular expression
+        import re
+        chr_val = re.findall(r'\d+$', chr_val)
+        if len(chr_val)==0:
+            print(f'#ERROR: Wrong format of chromosome number in original input file {args.input}')
+            print('# - Valid formats are: CHR1, chr1 or 1')
+            print(f'# - Current format is: {df[idx_chr][0]}')
+            print('#END')
+            exit()
+        else:
+            chr_text = df[idx_chr][0].split(chr_val)[0] # Get text portion in chromosome number for stripping
+            df[idx_chr] = df[idx_chr].apply(lambda x: x.split(chr_text)[-1])
+
+    # Fill rest columns with value of the first column
+    df.iloc[:, [idx_chr, idx_id, 0, idx_pos, 0, 0]].to_csv(output_fn, sep='\t', index=False, header=False)
+
+def ucsc_bed_to_text(input_fn, org_fn, output_fn):
+    '''
+    Convert UCSC BED format to original text file format
+    Parameters:
+    - input_fn: file in USCS BED format
+    - org_fn: Original txt file (pre liftover)
+    - output_fn
+    Return:
+    - Converted file is saved in output_fn
+    '''
+    idx_chr, idx_id, idx_pos = args.chr_id_pos.split(':')  # get indices of columns
+    idx_chr, idx_id, idx_pos = int(idx_chr), int(idx_id), int(idx_pos)
+    df_bed = pd.read_csv(input_fn, sep='\t', header=None)
+    # BED file use chr1, chr2, ... in chromosome column, need to strip 'chr' portion for merging
+    # 1st column in BED file is chromosome number
+    # Some chromosome number is changed to after liftover
+    df_bed[0] = df_bed[0].apply(lambda x: int(x.split('_')[0].split('chr')[-1]))
+    df_org = pd.read_csv(org_fn, sep='\t', header=None)
+    df_org.rename(columns={idx_pos:'-1'}) # Rename pos column to -1, so later we can replace it with liftovered values
+    # Check if org file also needs conversion of chromosome numbers to integers
+    org_chr_num = df_org[idx_chr][0]
+    try:
+        org_chr_num = int(org_chr_num)
+    except: # If chromosome number is not numeric
+        for chr_txt in ['chr', 'CHR']:
+            if chr_txt in org_chr_num:
+                df_org[idx_chr] = df_org[idx_chr].apply(lambda x: int(x.split(chr_txt)[-1]))
+    #TODO
+    # Might need to allow other text in the future
+
+    # In BED format 1st column is chromosome number, 4nd column is ID
+    df_bed.columns = [-1, -2, -3, -4] # Rename columns in df_bed so merge will not have columns with the same names
+    df_merged = df_bed.merge(df_org, left_on=[-1, -4], right_on=[idx_chr, idx_id])
+    df_merged_org = df_org.merge(df_merged[[idx_chr, idx_id]], on=[idx_chr, idx_id])
+    df_merged_org[idx_pos] = df_merged[-2] # Replace position values by liftovered values
+    df_merged_org.to_csv(output_fn, sep='\t', index=False, header=False)
+
+def convert():
+    print('\n#Convert input file to UCSC BED format for liftOver tool')
+    # Prepare file names
+    tmp_bed_fn = os.path.join(args.output_path, args.output.split('.')[0] + '.tmp.bed') # tmp BED file, created from input txt or plink bim file
+    converted_bed = os.path.join(args.output_path,
+                                 args.output.split('.')[0]) + '.converted.bed'  # Liftover converted BED file name
+    unlifted = os.path.join(args.output_path, args.output.split('.')[0]) + '.unlifted.bed'  # File name of unlifted positions
+    #TODO
+    # Has not test plink bim as input
     if args.input.endswith('.bim'): # Process plink file
-        plink_bim_to_ucsc_bed(args.input, args.output+'.bed')
-        ucsc_bed_to_plink_bim(args.output+'.bed', args.input, args.output)
-    else: # General text file as input
-        txt_to_bed()
+        plink_bim_to_ucsc_bed(os.path.join(args.output_path, args.input), tmp_bed_fn)
+        # Run liftover
+        # liftOver /data100t1/home/wanying/hla_analysis/data_and_doc/chr6_TopMed_imputed/converted_UCSC_bed.bed hg38ToHg19.over.chain.gz output.bed unlifted.bed
+        cmd = f'{args.liftover} {tmp_bed_fn} {args.chain_file} {converted_bed} {unlifted}'
+        subprocess.run(f'rm {tmp_bed_fn}'.split()) # remove tmp file
+        print(f'#Run liftover as:\n  {cmd}\n')
+        subprocess.run(cmd.split())
+        ucsc_bed_to_plink_bim(converted_bed,
+                              os.path.join(args.output_path, args.input),
+                              os.path.join(args.output_path, args.output))
+    else:  # General text file as input, uses tab as delimiter, no header
+        # tmp files might be removed later
+        tmp_plink_fn = os.path.join(args.output_path, args.output.split('.')[0]+'.tmp.bim') # Convert text file to .bim format
+        txt_to_plink_bim(args.input, tmp_plink_fn)
+        plink_bim_to_ucsc_bed(tmp_plink_fn, tmp_bed_fn)
+        # Run liftover
+        # liftOver /data100t1/home/wanying/hla_analysis/data_and_doc/chr6_TopMed_imputed/converted_UCSC_bed.bed hg38ToHg19.over.chain.gz output.bed unlifted.bed
+        cmd = f'{args.liftover} {tmp_bed_fn} {args.chain_file} {converted_bed} {unlifted}'
+        print(f'#Run liftover as:\n  {cmd}\n')
+        subprocess.run(cmd.split())
+        subprocess.run(f'rm {tmp_plink_fn}'.split()) # Remove tmp files
+        subprocess.run(f'rm {tmp_bed_fn}'.split())
+        # Convert liftovered BED file back to original format
+        ucsc_bed_to_text(converted_bed,
+                         os.path.join(args.output_path, args.input),
+                         os.path.join(args.output_path, args.output))
+    print('\n#Done converting input file to UCSC BED format\n')
 
-# Change input and output file names as needed
-input_fn = '/data100t1/home/wanying/hla_analysis/data_and_doc/chr6_TopMed_imputed/chr6_merged_raw.bim'
-output_fn = '/data100t1/home/wanying/hla_analysis/data_and_doc/chr6_TopMed_imputed/converted_UCSC_bed.bed'
-
-# plink_bim_to_ucsc_bed(input_fn, output_fn)  # Convert plink .bim file to UCSC BED format
-# Use this after processing
-# liftOver /data100t1/home/wanying/hla_analysis/data_and_doc/chr6_TopMed_imputed/converted_UCSC_bed.bed hg38ToHg19.over.chain.gz output.bed unlifted.bed
-
-input_bed_fn = '/data100t1/home/wanying/hla_analysis/data_and_doc/chr6_TopMed_imputed/output.bed'
-input_bim_fn = '/data100t1/home/wanying/hla_analysis/data_and_doc/chr6_TopMed_imputed/chr6_merged_raw.bim'
-output_fn = '/data100t1/home/wanying/hla_analysis/data_and_doc/chr6_TopMed_imputed/chr6_merged_raw_converted_hg19.bim'
-ucsc_bed_to_plink_bim(input_bed_fn, input_bim_fn, output_fn)
+if __name__ == '__main__':
+    convert()
--- src/process_args.py
+++ src/process_args.py
@@ -4,18 +4,28 @@ import os
 
 def process_args():
     dt = datetime.datetime.now().strftime('%Y-%m-%d')
-    parser = argparse.ArgumentParser(f'################# Start liftover on {dt} #################\n')
-    valid_args = ['--input', '--output', '--output_path', '--chain_file']
+    parser = argparse.ArgumentParser(f'\n################# Start liftover on {dt} #################\n')
+    valid_args = ['--input', '--output', '--output_path', '--chain_file', '--liftover', '--chr_id_pos']
     arg_info = {'--input': 'Input file in UCSC BED format (.bed), or plink file (.bim), or text file with one position per line.',
                 '--output': 'Output file name without suffix',
                 '--output_path': 'Path to output file. Default is the same as input file',
-                '--chain_file': 'Appropriate chain file to use'}
+                '--chain_file': 'Appropriate chain file to use',
+                '--liftover': 'Path to liftover tool',
+                '--chr_id_pos':'index of chromosome number, SNP id, position, separated by ":". for example: 1:3:5'}
     for arg in valid_args:
         if arg == '--output_path':
             parser.add_argument(arg, help=arg_info[arg], default='./')
+        elif arg == '--liftover': # For WZ's use only
+            parser.add_argument(arg, help=arg_info[arg], default='/data100t1/home/wanying/downloaded_tools/liftover/liftOver')
+        elif arg == '--chain_file': # For WZ's use only, B37 to B38
+            parser.add_argument(arg, help=arg_info[arg],
+                                default='/data100t1/home/wanying/downloaded_tools/liftover/hg19ToHg38.over.chain.gz')
         else:
             parser.add_argument(arg, help=arg_info[arg])
     args = parser.parse_args()
+    print('\n# Arguments used:')
+    for arg in valid_args:
+        print(f'# {arg}:', eval(f'args.{arg[2:]}'))
 
     # Sanity checks
     # Check input and chain file
@@ -23,15 +33,13 @@ def process_args():
         print(f'#Error: Input file does not exist: {args.input}\nExit')
         exit()
     if not os.path.isfile(args.chain_file):
-        print(f'#Error: Chain file does not exist: {args.input}\nExit')
+        print(f'#Error: Chain file does not exist: {args.chain_file}\nExit')
         exit()
 
     # Check output file path
     if not os.path.isdir(args.output_path):
         print('#Output directory does not exist. Making one')
         os.mkdir(args.output_path)
-    print('\n# Arguments used:')
-    for arg in valid_args:
-        print('# - ', eval('args.arg'))
-    arg.output = os.path.join(args.output_path, args.output) # Add path to output file name
+
+    # args.output = os.path.join(args.output_path, args.output) # Add path to output file name
     return args
